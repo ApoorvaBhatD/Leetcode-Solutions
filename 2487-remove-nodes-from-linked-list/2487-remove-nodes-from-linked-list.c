d3d898f5857126b7f520dd6d9dc4213fdadda0c8
ApoorvaBhatD <bhatapoorva24@gmail.com>
2025-11-09 23:36:45 +0530

    Time: 43 ms (43.19%), Space: 65.2 MB (12.9%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.c
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.c
@@ -1,63 +1,50 @@
-#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+ *
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+*/
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    while (curr) {
+        struct ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
 struct ListNode* removeNodes(struct ListNode* head) {
-   
-    int count = 0;
-    struct ListNode* temp = head;
-    while (temp) { count++; temp = temp->next; }
-
-    if (count == 0) return NULL;
-
-   
-    int* arr = malloc(count * sizeof(int));
-    temp = head;
-    for (int i = 0; i < count; i++) {
-        arr[i] = temp->val;
-        temp = temp->next;
-    }
     
-   
-    bool* keep = malloc(count * sizeof(bool));
-    for (int i = 0; i < count; i++) {
-        keep[i] = true;
-        for (int j = i + 1; j < count; j++) {
-            if (arr[j] > arr[i]) {
-                keep[i] = false;
-                break;
-            }
+    head = reverseList(head);
+
+    struct ListNode* curr = head;
+    int maxVal = curr->val;
+    struct ListNode* prev = curr;
+    curr = curr->next;
+
+    while (curr) {
+        if (curr->val < maxVal) {
+            
+            prev->next = curr->next;
+            free(curr);
+            curr = prev->next;
+        } else {
+            maxVal = curr->val;
+            prev = curr;
+            curr = curr->next;
         }
     }
 
    
-    struct ListNode* newHead = NULL;
-    struct ListNode* last = NULL;
-    temp = head;
-    for (int i = 0; i < count; i++) {
-        if (keep[i]) {
-            struct ListNode* node = malloc(sizeof(struct ListNode));
-            node->val = arr[i];
-            node->next = NULL;
-            if (!newHead) {
-                newHead = node;
-            } else {
-                last->next = node;
-            }
-            last = node;
-        }
-        temp = temp->next;
-    }
-
-   
-    free(arr);
-    free(keep);
-
-    return newHead;
+    head = reverseList(head);
+    return head;
 }
