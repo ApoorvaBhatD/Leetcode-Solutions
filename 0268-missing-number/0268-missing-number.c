b0236b56a509dc97dac67f3c244d6f384e73a3b6
ApoorvaBhatD <bhatapoorva24@gmail.com>
2026-04-27 23:08:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0268-missing-number/0268-missing-number.c
+++ 0268-missing-number/0268-missing-number.c
@@ -1,10 +1,9 @@
 int missingNumber(int* nums, int numsSize) {
-    int sum_n,sum_nums=0;
-    sum_n=numsSize*(numsSize+1)/2;
+    int sum_nums=0;
     for(int i =0;i<numsSize;i++){
         sum_nums=sum_nums+nums[i];
     }
-    return (sum_n - sum_nums);
+    return ((numsSize*(numsSize+1)/2) - sum_nums);
     
     
 }
