24094d1b4e2f6c0d264b033706fcbaef1f0e6571
ApoorvaBhatD <bhatapoorva24@gmail.com>
2026-07-21 12:38:44 +0530

    Time: 0 ms (100%), Space: 19.5 MB (54.6%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if (n==0):
+            return 1.0
+        if (n==1):
+            return x
+
+        if (n<0):
+            x=1/x
+            n=-n
+        result = 1.0
+        prod=x
+        while n>0:
+            if n%2==1:
+                result*=prod
+            prod*=prod
+            n//=2
+        return result
