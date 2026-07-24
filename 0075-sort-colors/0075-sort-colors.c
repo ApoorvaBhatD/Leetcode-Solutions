fb2050db728bb30a56b9af7773e6ac1d0c83f0cf
ApoorvaBhatD <bhatapoorva24@gmail.com>
2026-07-24 23:33:22 +0530

    Time: 0 ms (100%), Space: 10.5 MB (41.16%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.c
@@ -0,0 +1,15 @@
+void sortColors(int* nums, int numsSize) {
+    int c0=0,c1=0,c2=0;
+    for (int i=0;i<numsSize;i++){
+        if (nums[i]==0) c0++;
+        if (nums[i]==1) c1++;
+        if (nums[i]==2) c2++;
+    }
+    c1=c1+c0;
+    c2=c2+c1;
+    for (int i=0;i<numsSize;i++){
+        if (i<c0) {nums[i]=0;}
+        else if (i<c1){ nums[i]=1;}
+        else if(i<c2) {nums[i]=2;}
+    }
+}
