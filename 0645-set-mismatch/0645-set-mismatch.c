d53948733a5e3d88d75f3282f26921033e6f5079
ApoorvaBhatD <bhatapoorva24@gmail.com>
2026-08-10 20:58:30 +0530

    Time: 0 ms (100%), Space: 10.8 MB (16.98%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.c
@@ -0,0 +1,17 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findErrorNums(int* nums, int numsSize, int* returnSize) {
+    int* count = (int*)calloc(numsSize + 1, sizeof(int));
+    int *result=(int *)malloc(2*sizeof(int));
+    *returnSize=2;
+    for (int i=0;i<numsSize;i++)count[nums[i]]++;
+
+    for(int i=1;i<=numsSize;i++){
+        if (count[i]==2) result[0]=i;
+        if (count[i] == 0) result[1] = i;
+        
+    }
+    free(count);
+    return result;
+}
