65c0c80ace4dabb76c94754608aca2175587baef
ApoorvaBhatD <bhatapoorva24@gmail.com>
2026-08-22 13:03:34 +0530

    Time: 7 ms (16.8%), Space: 19.1 MB (98.63%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -1,16 +1,5 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        dictn={}
-        arr=[]
-        
-        for n in nums :
-            dictn[n]=dictn.get(n,0)+1
-        high=max(nums)
-        low=min(nums)
-        i=low
-        while (i<=high):
-            if i not in dictn :
-                arr.append(i)
-            i+=1
-        return arr
-        
+        #nums_set=set(nums)
+        low,high = min(nums),max(nums)
+        return [i for i in range (low,high+1) if i not in nums]
