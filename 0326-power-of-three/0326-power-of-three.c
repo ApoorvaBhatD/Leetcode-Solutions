8ac38b2e7175efb7389ddfa8eb55eb036e1f1537
APOORVA BHAT D <bhatapoorva24@gmail.com>
2026-09-14 22:09:09 +0530

    Time: 3 ms (56.14%), Space: 9.1 MB (77.28%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.c
+++ 0326-power-of-three/0326-power-of-three.c
@@ -1,10 +1,3 @@
 bool isPowerOfThree(int n) {
-    if (n<=0) return false;
-    long long x=1;
-    for (int i = 0; x<n ; i++){
-        x=x*3;
-    }
-    if (x==n) return true;
-    return false;
-    
+    return n > 0 && 1162261467 % n == 0;
 }
